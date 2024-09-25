3c2cf53fa56906abcc0f77d3eeeb536c89e7b5dd
sshojib <sashojib57@gmail.com>
2024-09-25 20:28:38 +0600

    my fifth commit

--- js/script.js
+++ js/script.js
@@ -1,127 +1,161 @@
+
+
+
+
+
+
+
+
+// blog-button
+document.getElementById('startBtn').addEventListener('click', function() {
+    document.getElementById('quizContainer').classList.remove('hidden');
+    loadQuestions();
+});
+function loadQuestions() {
+    const questions = [
+        " What is DOM?",
+        "How do you select an element from the DOM?",
+        "What is event delegation in the contaxt of the DOM?",
+        "How do you manipulate an element's attributes an styles using the DOM?"
+    ];
+
+    const questionsContainer = document.getElementById('questions');
+    questions.forEach((question, index) => {
+        const questionP = document.createElement('p');
+        questionP.className = 'text-left';
+        questionP.innerHTML = `<label>${question}</label>
+        <input type="text" id="answer${index}" class=" px-2 py-1" placeholder="Your answer"></input>
+        `;
+        
+        questionsContainer.appendChild(questionP);
+    });
+}
+// hidden-part
+document.getElementById('history-tab').addEventListener('click', function() {
+    document.getElementById('historyList').classList.remove('hidden');
+});
+
+document.getElementById('donation-tab').addEventListener('click', function() {
+    document.getElementById('historyList').classList.add('hidden');
+});
+// history-button-donation-button
+document.getElementById('history-tab').addEventListener('click', function() {
+    document.getElementById('donate-bangladesh').classList.add('hidden');
+    document.getElementById('historyList').classList.remove('hidden');
+});
+
+document.getElementById('donation-tab').addEventListener('click', function() {
+    document.getElementById('historyList').classList.add('hidden');
+    document.getElementById('donate-bangladesh').classList.remove('hidden'); 
+});
+
+
+// button-color-change
 const historyTab = document.getElementById('history-tab');
 const donationTab = document.getElementById('donation-tab');
 historyTab.addEventListener('click', function(){
     historyTab.classList.add('bg-[#B4F461]');
     donationTab.classList.remove('bg-[#B4F461]');
-});
-// 1 number
-const butonDonateNow = document.getElementById('donate-now');
-butonDonateNow.addEventListener('click', function(event){
-    event.preventDefault();
-    const inputAmount = document.getElementById('input-add-amount').value;
-    console.log(inputAmount);
-
-    if(inputAmount > 0){
-        alert('Thank you for your donation.')
-        const taka = document.getElementById('add-amount').innerText
-        console.log(taka);
-        const number = parseFloat(inputAmount);
-        const number2 = parseFloat(taka);
-        const totalAmount = number + number2
-        console.log(totalAmount);
-        document.getElementById('add-amount').innerText = totalAmount;
-    }
-    else{
-        alert('Please enter a valid donation amount.')
-    }
-})
-
-// 2 number
-const buttonDonateNow = document.getElementById('button-donate-now');
-buttonDonateNow.addEventListener('click', function(event){
-    event.preventDefault();
-    const inputAmount = document.getElementById('input-amount-add').value;
-    console.log(inputAmount);
-
-    if(inputAmount > 0){
-        alert('Thank you for your donation.')
-        const taka = document.getElementById('balance-add').innerText
-        console.log(taka);
-        const number = parseFloat(inputAmount);
-        const number2 = parseFloat(taka);
-        const totalAmount = number + number2
-        console.log(totalAmount);
-        document.getElementById('balance-add').innerText = totalAmount;
-    }
-    else{
-        alert('Please enter a valid donation amount.')
-    }
-})
-
-// 3 number
-const btnDonateNow = document.getElementById('btn-donate-now');
-btnDonateNow.addEventListener('click', function(event){
-    event.preventDefault();
-    const inputMoney = document.getElementById('input-amount').value;
-    console.log(inputMoney);
-
-    if(inputMoney > 0){
-        alert('Thank you for your donation.')
-        const balance = document.getElementById('balance').innerText
-        console.log(balance);
-        const number = parseFloat(inputMoney);
-        const number2 = parseFloat(balance);
-        const newBalance = number + number2
-        console.log(newBalance);
-        document.getElementById('balance').innerText = newBalance;
-    }
-    else{
-        alert('Please enter a valid donation amount.')
-    }
-});
-
-
-
-// 1 number
-const btonDonateNow = document.getElementById('donate-now');
-btonDonateNow.addEventListener('click', function(event){
-    event.preventDefault();
-    const inputMoney = document.getElementById('input-add-amount').value;
-    console.log(inputMoney);
 
     
-        const balance = document.getElementById('current-amount').innerText
-        console.log(balance);
-        const number = parseFloat(inputMoney);
-        const number2 = parseFloat(balance);
-        const newBalance = number - number2
-        console.log(newBalance);
-        document.getElementById('current-amount').innerText = newBalance;
-
+    document.getElementById('donate-bangladesh').classList.add('hidden');
+    
 });
-// 2 number
-const butnDonateNow = document.getElementById('button-donate-now');
-butnDonateNow.addEventListener('click', function(event){
+const donateTab = document.getElementById('donation-tab');
+donateTab.addEventListener('click', function(event){
+    donateTab.classList.add('bg-[#B4F461]');
+    historyTab.classList.remove('bg-[#B4F461]');
+
+})
+  
+
+// 1
+
+
+const buttonDonateNow = document.getElementById('donate-now')
+buttonDonateNow.addEventListener('click', function(event) {
+event.preventDefault();
+    let currentAmount = parseFloat(document.getElementById('current-amount').innerText);
+    let donationAmount = parseFloat(document.getElementById('input-add-amount').value);
+    
+    
+    if (donationAmount > 0 && donationAmount <= currentAmount) {
+         alert('Thank you for your donation.')
+        currentAmount -= donationAmount;
+        document.getElementById('current-amount').textContent = currentAmount;
+        document.getElementById('input-add-amount').value = '';
+        document.getElementById('add-amount').innerText = donationAmount;
+    } else {
+        alert("Please enter a valid donation amount.");
+    }
+});
+// 2
+const butnDonateNow = document.getElementById('button-donate-now') 
+butnDonateNow.addEventListener('click', function(event) {
     event.preventDefault();
-    const inputMoney = document.getElementById('input-amount-add').value;
-    console.log(inputMoney);
+    let currentAmount = parseFloat(document.getElementById('current-amount').innerText);
+    let donationAmount = parseFloat(document.getElementById('input-amount-add').value);
+    
+    
+    if (donationAmount > 0 && donationAmount <= currentAmount) {
+        alert('Thank you for your donation.')
+        currentAmount -= donationAmount;
+        document.getElementById('current-amount').textContent = currentAmount;
+        document.getElementById('input-amount-add').value = '';
+        document.getElementById('balance-add').innerText = donationAmount;
+    } else {
+        alert("Please enter a valid donation amount.");
+    }
+});
+// 3
+const btnDonateNow = document.getElementById('btn-donate-now')
+btnDonateNow.addEventListener('click', function(event) {
+event.preventDefault();
+    let currentAmount = parseFloat(document.getElementById('current-amount').innerText);
+    let donationAmount = parseFloat(document.getElementById('input-amount').value);
+    
+    if (donationAmount > 0 && donationAmount <= currentAmount) {
+        alert('Thank you for your donation.')
+        currentAmount -= donationAmount;
+        document.getElementById('current-amount').textContent = currentAmount;
+        document.getElementById('input-amount').value = '';
+        document.getElementById('balance').innerText = donationAmount;
+    } else {
+        alert("Please enter a valid donation amount.");
+    }
+});
+// //////s
+document.getElementById('donate-now').addEventListener('click', function() {
+    
+    let donationAmount = parseInt(document.getElementById('input-add-amount').value);
+    
+    
+    let currentDate = new Date().toLocaleString();
 
     
-        const balance = document.getElementById('current-amount').innerText
-        console.log(balance);
-        const number = parseFloat(inputMoney);
-        const number2 = parseFloat(balance);
-        const newBalance = number - number2
-        console.log(newBalance);
-        document.getElementById('current-amount').innerText = newBalance;
+    if (donationAmount > 0) {
+        
+        let historyItem = document.createElement('li');
+        historyItem.className = 'bg-white rounded-lg shadow-lg p-5 mx-auto mt-4 w-[900px] font-semibold';
+        historyItem.innerHTML = `${donationAmount} Taka is donated for Donate for Flood at Noakhali, Bangladesh
+            <p class="text-[#4e4e4e]">Date: ${currentDate}</p>`;
 
+        
+        document.getElementById('historyList').appendChild(historyItem);
+
+        
+        document.getElementById('input-add-amount').value = '';
+    }
+});
+document.getElementById('history-tab').addEventListener('click', function() {
+    const historyList = document.getElementById('historyList');
+    historyList.classList.toggle(''); 
 });
 
-// 3 number
-const bttnDonateNow = document.getElementById('btn-donate-now');
-bttnDonateNow.addEventListener('click', function(event){
-    event.preventDefault();
-    const inputMoney = document.getElementById('input-amount').value;
-    console.log(inputMoney);
 
-        const balance = document.getElementById('current-amount').innerText
-        console.log(balance);
-        const number = parseFloat(inputMoney);
-        const number2 = parseFloat(balance);
-        const newBalance = number - number2
-        console.log(newBalance);
-        document.getElementById('current-amount').innerText = newBalance;
-});
+
+
+
 
 
 
